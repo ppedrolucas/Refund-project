23112b43bf3a4f6fe5340bed152e734463937587
ppedrolucas <pp15lucas@gmail.com>
2025-04-17 23:11:17 -0300

    Criando o ícone de remover item da lista.

--- script.js
+++ script.js
@@ -1,7 +1,96 @@
 //Seleciona os elementos do formulário
 const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+const form = document.querySelector("form");
+const expenseList = document.querySelector("ul");
 
 amount.oninput = () => {
   //Evento que observa qualquer interação com o input
-  
+
+  // Validação do input para aceitar somente números. Primeiro obtém o valor atual e ignora as letras inseridas no input.
+  let value = amount.value.replace(/\D/g, "");
+
+  // Converte o valor do input de String para Number e o divide por 100 para transformar em centavos.
+  value = Number(value) / 100;
+
+  //Depois atualiza esse valor formatado para o mesmo input. E claro, usando uma expressão regular.
+  amount.value = formatCurrencyBRL(value);
 };
+
+function formatCurrencyBRL(value) {
+  // Formata o valor para o real brasileiro
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  return value;
+}
+
+// Capitura o evento de submit para obter os valores
+form.onsubmit = (e) => {
+  // Previne o evento de reload da página
+  e.preventDefault();
+
+  // Cria um objeto com os detalhes na nova despesa
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  };
+
+  // Chama a função de adicionar item à lista
+  expenseAdd(newExpense);
+};
+
+function expenseAdd(newExpense) {
+  try {
+    // Cria o elemento de li para adicionar à lista
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+
+    // Cria o icone da categoria
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.catecory_name);
+
+    // Cria a informação da despesa
+    const expenseInfo = document.createElement("div");
+    expenseInfo.classList.add("expense-info");
+
+    const expenseName = document.createElement("strong");
+    expenseName.textContent = newExpense.expense;
+
+    // Cria a categoria da despesa
+    const expenseCategory = document.createElement("span");
+    expenseCategory.textContent = newExpense.category_name;
+
+    // Adicionar name e category em expenseInfo
+    expenseInfo.append(expenseName, expenseCategory);
+
+    const expenseAmount = document.createElement("span");
+    expenseAmount.classList.add("expense-amount");
+    expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount
+      .toUpperCase()
+      .replace("R$", "")}`;
+
+    // Cria o ícone de remover
+    const removeIcon = document.createElement("img");
+    removeIcon.classList.add("remove-icon");
+    removeIcon.setAttribute("src", "img/remove.svg");
+    removeIcon.setAttribute("alt", "remover");
+
+    // Adiciona as informações no item
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon);
+
+    // Adiciona item à lista
+    expenseList.append(expenseItem);
+  } catch (e) {
+    alert("Não foi possível atualizar a lista de despesas!");
+    console.log(e);
+  }
+}
